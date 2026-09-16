deb933b48b6be1490b1888d420f0302d6ad6828e
Yevhenii1951 <yevhenii1951@users.noreply.github.com>
2026-09-16 15:33:36 +0000

    fix(auth): signInSchema Fehlermeldungen auf Deutsch, korrigierte Laenge

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -2,13 +2,13 @@ import { object, string, number } from "zod";
 import { z } from "zod";
 
 export const signInSchema = object({
-  email: string({ required_error: "Email is required" })
-    .min(1, "Email is required")
-    .email("Invalid email"),
-  password: string({ required_error: "Password is required" })
-    .min(1, "Password is required")
-    .min(6, "Password must be more than 8 characters")
-    .max(32, "Password must be less than 32 characters")
+  email: string({ required_error: "E-Mail ist erforderlich" })
+    .min(1, "E-Mail ist erforderlich")
+    .email("Ungültige E-Mail-Adresse"),
+  password: string({ required_error: "Passwort ist erforderlich" })
+    .min(1, "Passwort ist erforderlich")
+    .min(6, "Das Passwort muss mindestens 6 Zeichen lang sein")
+    .max(32, "Das Passwort darf höchstens 32 Zeichen lang sein")
 });
 
 export const ingredientSchema = object({
