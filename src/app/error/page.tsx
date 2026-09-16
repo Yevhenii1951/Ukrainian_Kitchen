e5d278246f534124a5f23b841ba1c36d25017841
Yevhenii1951 <yevhenii1951@users.noreply.github.com>
2026-09-16 15:45:58 +0000

    feat(error): Zurück-Button auf der Fehlerseite

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { Button } from "@heroui/react";
+import Link from "next/link";
 
 const ErrorPage = () => {
   const searchParams = useSearchParams();
   const message = searchParams.get("message") || "Unbekannter Fehler";
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-4">
       <p className="text-red-500 text-xl">{message}</p>
+      <Button as={Link} color="primary" variant="shadow" href="/">
+        Zurück zur Startseite
+      </Button>
     </div>
   );
 };
